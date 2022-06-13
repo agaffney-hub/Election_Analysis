4a1cfe77018cbe7e78c66872be8f3c7071c2a951
agaffney-hub <amyjonaye@gmail.com>
2022-06-13 00:53:05 -0700

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,26 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+
+
+# Import the datetime class from the datetime module.
+#import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+#now = dt.datetime.now()
+# Print the present time.
+#print("The time right now is ", now)
+
+
